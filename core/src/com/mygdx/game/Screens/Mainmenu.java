1892234263ed2cc2135b240feb42dcc932d16687
Hoang Long <trinhhoanglong200@gmail.com>
2021-04-08 12:12:16 +0700

    Add toast

--- core/src/com/mygdx/game/Screens/Mainmenu.java
+++ core/src/com/mygdx/game/Screens/Mainmenu.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -89,6 +91,34 @@ public class Mainmenu implements Screen {
         stage.addActor(Exit);
 
     }
+    //
+    public void showDialog(String s){
+        final Dialog a = new Dialog("Warning",skin);
+        a.setWidth(200);
+        a.setHeight(150);
+        a.setPosition(Main.WIDTH/2-a.getWidth()/2,Main.HEIGHT/2-a.getHeight()/2);
+
+        Label label = new Label(s,skin,"optional");
+
+        a.getContentTable().add(label).center().expand();
+        TextButton btn = new TextButton("OK", skin, "round");
+        a.getButtonTable().add(btn);
+        btn.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                a.remove();
+
+            }
+        });
+        stage.addActor(a);
+
+    }
     @Override
     public void show() {
 
@@ -98,7 +128,7 @@ public class Mainmenu implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        stage.act();
         stage.draw();
 
 
@@ -166,13 +196,11 @@ public class Mainmenu implements Screen {
                     }
                     else
                     {
-                        JOptionPane.showMessageDialog(new JFrame(), "Player's name existed", "Warning", JOptionPane.WARNING_MESSAGE);
-
+                        showDialog("Username is taken");
                     }
                 }
                 else{
-                    JOptionPane.showMessageDialog(new JFrame(), "Please fill out the box", "Warning", JOptionPane.WARNING_MESSAGE);
-
+                    showDialog("Please fill out the box");
                 }
 
             }
